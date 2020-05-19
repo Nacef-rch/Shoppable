6a1f9bcedba64dce3edd8311e8e1d1a19064cd75
Nacef Racheh <rachehnacef@gmail.com>
2020-05-19 20:34:23 +0100

    feat(add-comp): adding login component

--- projects/authentication/src/lib/authentication.module.ts
+++ projects/authentication/src/lib/authentication.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthenticationComponent } from './authentication.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './modules/login/components/login.component';
 
 @NgModule({
-    declarations: [AuthenticationComponent],
+    declarations: [AuthenticationComponent, LoginComponent],
     imports: [HttpClientModule, BrowserModule, CommonModule],
-    exports: [AuthenticationComponent],
+    exports: [AuthenticationComponent, LoginComponent],
 })
 export class AuthenticationModule {}
