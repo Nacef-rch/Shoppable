0ba8f6b79cee2043387e66ee4123da87f09c2a15
Nacef Racheh <rachehnacef@gmail.com>
2020-05-19 17:43:21 +0100

    fix: cleaning code

--- projects/authentication/src/lib/authentication.module.ts
+++ projects/authentication/src/lib/authentication.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthenticationComponent } from './authentication.component';
-import { TestComponent } from './test/test.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthenticationDirective } from './authentication.directive';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [AuthenticationComponent, TestComponent, AuthenticationDirective],
+    declarations: [AuthenticationComponent, AuthenticationDirective],
     imports: [HttpClientModule, BrowserModule, CommonModule],
-    exports: [AuthenticationComponent, TestComponent],
+    exports: [AuthenticationComponent],
 })
 export class AuthenticationModule {}
