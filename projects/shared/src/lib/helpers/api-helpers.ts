cf88dbb7f4e0b2a41214dc5c07c5cb6427f00c28
Nacef Racheh <rachehnacef@gmail.com>
2020-06-01 07:50:07 +0100

    refactor: refactoring code

--- projects/shared/src/lib/helpers/api-helpers.ts
+++ projects/shared/src/lib/helpers/api-helpers.ts
@@ -3,33 +3,17 @@ export const handleError = (errorRes: any): string => {
     if (!errorRes.error) {
         return errorMessage;
     }
-    if (errorRes.error.name) {
-        errorMessage = 'Must not be empty';
-    }
+
     if (errorRes.error.email) {
-        switch (errorRes.error.email) {
-            case 'Must not be empty':
-                errorMessage = 'Must not be empty';
-                break;
-            case 'Email is already in use':
-                errorMessage = 'Email is already in use';
-        }
+        errorMessage = 'Email is already in use !';
     }
-    if (errorRes.error.password) {
-        errorMessage = 'Must not be empty';
-    }
+
     if (errorRes.error.confirmPassword) {
-        errorMessage = 'Passwords must match';
+        errorMessage = 'Passwords must match !';
     }
 
     if (errorRes.error.handle) {
-        switch (errorRes.error.handle) {
-            case 'Must not be empty':
-                errorMessage = 'Must not be empty';
-                break;
-            case 'this handle is already taken':
-                errorMessage = 'This handle is already taken';
-        }
+        errorMessage = 'This handle is already taken !';
     }
     if (errorRes.error.general) {
         errorMessage = 'Wrong credentials, please try again !';
