5edd242748e6bed82d65d12692d94fe6082c4dd9
Nacef Racheh <rachehnacef@gmail.com>
2020-06-20 19:16:10 +0100

    refactor: refactoring code

--- projects/store/src/lib/Containers/template/default/default.component.ts
+++ projects/store/src/lib/Containers/template/default/default.component.ts
@@ -12,19 +12,29 @@ import { StoreProducts } from '@product/models/product.model';
 export class DefaultComponent implements OnInit {
     public Products$: Observable<StoreProducts[]> = this.productFacade.storeProducts$;
     products: StoreProducts[];
+    productsArray: StoreProducts[];
+    categories = [];
     public isLoading$: Observable<boolean> = this.productFacade.loading$;
     constructor(private productFacade: ProductFacade) {}
 
     ngOnInit(): void {
         this.productFacade.fetchStoreStart();
+
         this.Products$.subscribe((resData) => {
             this.products = resData;
-            console.log(this.products);
+            this.productsArray = resData;
+            resData.forEach((data) => {
+                this.categories.push(data.categoryId);
+
+                this.categories.splice(0, this.categories.length, ...new Set(this.categories));
+            });
         });
     }
-    onClick() {
+    onClick(category) {
+        this.products = this.productsArray;
+        console.log(category);
         const newArray = this.products.filter(function (el) {
-            return el.categoryId == 'Men';
+            return el.categoryId == category;
         });
         this.products = newArray;
         console.log(this.products);
