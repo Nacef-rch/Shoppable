f229a26eb1073bf530508b9be6ca001217c16a52
Nacef Racheh <rachehnacef@gmail.com>
2020-06-22 02:20:10 +0100

    refactor: refactoring code

--- projects/layout/src/lib/Components/product-info/default/default-product-info/default-product-info.component.ts
+++ projects/layout/src/lib/Components/product-info/default/default-product-info/default-product-info.component.ts
@@ -13,42 +13,49 @@ import { StoreProducts } from '@product/models/product.model';
 export class DefaultProductInfoComponent implements OnInit, OnDestroy {
     public Products$: Observable<StoreProducts[]> = this.productFacade.storeProducts$;
     paramsSubscription: Subscription;
+    productSubscription: Subscription;
     productQuantity = 1;
     products: StoreProducts[];
     productId;
     category;
-    like = true;
+    like = false;
     product: StoreProducts = null;
-    constructor(private route: ActivatedRoute, private productFacade: ProductFacade) {}
+    constructor(
+        private route: ActivatedRoute,
+        private productFacade: ProductFacade,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {
         this.paramsSubscription = this.route.params.subscribe((params: Params) => {
             this.productId = params['id'];
             this.category = params['category'];
-        });
-        this.Products$.subscribe((resData) => {
-            resData.forEach((product, index, array) => {
-                if (product.productId == this.productId) {
-                    this.product = product;
-                }
-                if (index === array.length - 1) {
-                    if (!this.product) {
-                        window.alert('product not found !!');
-                    }
-                }
-            });
-            this.products = resData;
 
-            this.products = this.products.filter((item) => {
-                if (item.categoryId == this.category && item.productId !== this.productId) {
-                    return item;
-                }
+            this.productSubscription = this.Products$.subscribe((resData) => {
+                resData.forEach((product, index, array) => {
+                    if (product.productId == this.productId) {
+                        this.product = product;
+                    }
+                    if (index === array.length - 1) {
+                        if (!this.product) {
+                            window.alert('product not found !!');
+                        }
+                    }
+                });
+                this.products = resData;
+
+                this.products = this.products.filter((item) => {
+                    if (item.categoryId == this.category && item.productId !== this.productId) {
+                        return item;
+                    }
+                });
             });
         });
     }
 
     ngOnDestroy() {
         this.paramsSubscription.unsubscribe();
+        this.productSubscription.unsubscribe();
     }
     clickPlus() {
         this.productQuantity++;
@@ -60,5 +67,12 @@ export class DefaultProductInfoComponent implements OnInit, OnDestroy {
     }
     onLike() {
         this.like = !this.like;
+    }
+    otherProductClicked(category, product) {
+        this.router.navigate([`store/shop/${category}/${product}`]);
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        this.like = false;
+        this.productQuantity = 1;
     }
 }
